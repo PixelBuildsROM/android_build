1e3661d744a3e2c65e2b92bc0575af7d94de34dc
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add PixelBuilds config.mk support
    [kdrag0n: Extracted from "build: Add Lineage build support"]
    
    Change-Id: I1fe5f97507a34d3951679d5dafe0d34e1781fab5

--- core/config.mk
+++ core/config.mk
@@ -1303,4 +1303,7 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+# Include any vendor specific config.mk file
+-include vendor/*/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
