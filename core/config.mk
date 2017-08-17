a691c4d3324f182f3fef27ab5943973c0029d42f
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add PixelBuilds build support
    Partial squash of the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include Lineage pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use lineage pathmap
    
        even if we're not building a lineage target, we need to
        use the lineage pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date:   Tue Jan 22 15:50:52 2019 +0100
    
        Don't export BUILD_NUMBER in envsetup
    
        * The value is meant to be set only before running an /official/ build
        * This allows us to execute consecutive make commands without
          running kati for every invocation
    
      Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    
      Author: Rashed Abdel-Tawab <rashed@linux.com>
      Date:   Thu Sep 5 21:35:38 2019 -0700
    
        core: Only include Lineage SE policy if repo exists
    
      Change-Id: If799106d2da6d8c303430e316edff365a22f7c99
    
      Author: dianlujitao <dianlujitao@lineageos.org>
      Date:   Thu Sep 5 21:56:36 2019 +0800
    
        product: Exclude AOSP apn list on Lineage build
    
        Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898
    
      Author: Michael Bestas <mkbestas@lineageos.org>
      Date:   Fri Apr 12 02:10:12 2019 +0300
    
        build: Conditionally include AudioPackage14.mk instead of AllAudio.mk
    
        * AllAudio.mk doesn't contain latest material files and also
          contains multiple duplicate files. Just copy the latest
          material ones and we'll include the older files in vendor/lineage
    
        Change-Id: Ic7353216a3f3dc3c51006d951ac932cfd6e5f3b5
    
      Author: dhacker29 <dhackerdvm@gmail.com>
      Date:   Sat Jun 6 22:22:05 2015 -0400
    
        Remove LatinIME as it is included per target in vendor/lineage
    
        Change-Id: Ibce4a244ec0653a7c7798d864047ac667866a1dc
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Oct 7 05:10:56 2019 -0500
    
        build: Remove su inclusion
    
        This stops including su in all lineage userdebug/eng builds.
        Inclusion of su is conditionally handled in /vendor/lineage.
    
        Change-Id: Ia3ad978146f170a2b260d77afe9eb580d4b4f823
    
      Author: Michael Bestas <mkbestas@lineageos.org>
      Date:   Sat Nov 7 22:45:12 2020 +0200
    
        Stop including vendor specific clear_vars.mk
    
        vendor/lineage/build/core/clear_vars.mk is no-op since 16.0
    
        Change-Id: I7e159cc9ed5a838c9a04c1a8b7dcf925d83f0e20
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- core/config.mk
+++ core/config.mk
@@ -407,6 +407,10 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(PB_BUILD),)
+include vendor/pb/config/BoardConfigPb.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1292,6 +1296,14 @@ dont_bother_goals := out product-graph
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
+
+ifneq ($(PB_BUILD),)
+ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+endif
+endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
--- envsetup.sh
+++ envsetup.sh
@@ -204,6 +204,13 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^aosp_") ; then
+        PB_BUILD=$(echo -n $1 | sed -e 's/^aosp_//g')
+    else
+        PB_BUILD=
+    fi
+    export PB_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -818,6 +825,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
